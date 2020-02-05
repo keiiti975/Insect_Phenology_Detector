c3b624394881f01752c36283e97756e791d85626
keiiti975 <gtselfmind2317@gmail.com>
2020-02-05 12:51:46 +0900

    modify compare_correction

--- evaluation/classification/evaluate.py
+++ evaluation/classification/evaluate.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 
 
-def accuracy(model, x, y, bs, return_correction_term=False):
+def accuracy(model, x, y, bs, return_correction_term=False, low_trainable_correction=False):
     """
         accuracy for all class
         - model <torch.model>
@@ -10,6 +10,7 @@ def accuracy(model, x, y, bs, return_correction_term=False):
         - y <Tensor[int]> : Tensor[label]
         - bs <int>
         - return_correction_term <bool> : (default=False)
+        - low_trainable_correction <bool> : (default=False)
     """
     model.eval()
     result_a = []
@@ -35,7 +36,7 @@ def accuracy(model, x, y, bs, return_correction_term=False):
     if return_correction_term is False:
         return correct.mean()
     else:
-        return correct.mean(), get_correction_term(result_a, y)
+        return correct.mean(), get_correction_term(result_a, y, low_trainable_correction)
 
 
 def confusion_matrix(model, x, y, labels, bs):
@@ -69,11 +70,12 @@ def confusion_matrix(model, x, y, labels, bs):
     return np.asarray(confusion_matrix).astype("float")
 
 
-def get_correction_term(result_a, y):
+def get_correction_term(result_a, y, low_trainable_correction=False):
     """
         get correction term for loss function
         - result_a <Array[int]> : Array[label]
         - y <Array[int]> : Array[label]
+        - low_trainable_correction <bool> : (default=False)
     """
     correction_term = []
     idx, count = np.unique(y, return_counts=True)
@@ -82,4 +84,19 @@ def get_correction_term(result_a, y):
         result_a_per_class = result_a[msk]
         y_per_class = y[msk]
         correction_term.append(1 / (result_a_per_class == y_per_class).mean())
+    
+    if low_trainable_correction == True:
+        correction_term = np.asarray(correction_term)
+        sorted_correction_term_index = np.argsort(correction_term)
+        reverse_index = np.flip(sorted_correction_term_index)
+        count = 0
+        index_num = len(idx)
+        for low_index in reverse_index:
+            if count < int(index_num * (2 / 3)):
+                correction_term[low_index] = 1.
+                count += 1
+            else:
+                correction_term[low_index] = 0.
+                count += 1
+            
     return torch.from_numpy(np.asarray(correction_term)).cuda().float()
